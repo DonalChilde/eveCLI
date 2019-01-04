9bb8d26728940a2626de3b9185d0013f0d357f7c
Chad <39224079+DonalChilde@users.noreply.github.com>
2019-01-04 09:49:34 -0700

    misc cleanup, moved filename generation to function

--- eveCli/esiMarketPrices.py
+++ eveCli/esiMarketPrices.py
@@ -5,7 +5,7 @@ import logging
 from pathlib import Path
 from datetime import datetime
 from typing import List
-from eveCli.utils import saveToFile
+from localUtils import saveToFile
 # import utils
 
 
@@ -53,9 +53,9 @@ class EsiMarketPrices(object):
 
     def formatData(self, data, outputFormat):
         if outputFormat == 'json':
-            return ((self.formatJson(data),'json'),)
+            return ((self.formatJson(data), 'json'),)
         if outputFormat == 'csv':
-            return ((self.formatCsv(data),'csv'),)
+            return ((self.formatCsv(data), 'csv'),)
         if outputFormat == 'both':
             return ((self.formatJson(data), 'json'), (self.formatCsv(data), 'csv'))
 
@@ -65,6 +65,7 @@ class EsiMarketPrices(object):
     def formatCsv(self, data):
         csvData = self.convertToCsv(data)
         return csvData
+
     def convertToCsv(self, data):
         raise NotImplementedError
 
@@ -115,39 +116,35 @@ class MarketPricesCmdLineParser(object):
             data, parsedCommands.outputFormat)
         for item in formattedData:
             print(item[0])
-        # print(formattedData[0])
+
+    def getFilename(self, parsedCommands):
+        filename = f"{FILENAME}_{DATETIMESTRING}"
+        return filename
 
     def saveToFile(self, parsedCommands):
-        # print('saveToFile')
         path = parsedCommands.outputFolderPath
         if parsedCommands.outputFileName:
             filename = parsedCommands.outputFileName
         else:
-            filename = f"{FILENAME}_{DATETIMESTRING}"
-        internalParams = {'filename':filename,'path':path,'formatter':self.target,'format':parsedCommands.outputFormat}
-        callback=saveMarketPrice
-        request = self.target.buildRequest(internalParams=internalParams,callback=callback)
+            filename = self.getFilename(parsedCommands)
+        internalParams = {'filename': filename, 'path': path,
+                          'formatter': self.target, 'format': parsedCommands.outputFormat}
+        callback = saveMarketPrice
+        request = self.target.buildRequest(
+            internalParams=internalParams, callback=callback)
         self.target.getRequests((request,))
-        
 
-        # callback = AQR.saveFileCallback
-        pass
 
 async def saveMarketPrice(state: AQR.RequestState) -> AQR.RequestState:
-    # print('made it to callback')
     outputFormat = state.action.internalParams['format']
     path = state.action.internalParams['path']
     filenameRoot = state.action.internalParams['filename']
-    formattedData = state.action.internalParams['formatter'].formatData(state.responseText,outputFormat)
-    # print(state.action.response.status)
-    # print(formattedData)
+    formattedData = state.action.internalParams['formatter'].formatData(
+        state.responseText, outputFormat)
     for item in formattedData:
-        # print(item[0])
         filename = filenameRoot + '.'+item[1]
-        saveToFile(path,filename,item[0])
+        saveToFile(path, filename, item[0])
     return state
-
-
 
 
 if __name__ == '__main__':
